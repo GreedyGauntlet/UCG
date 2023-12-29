dcf493c8fb0c2a587e68e5d79179f51a02ca06cb
Jason Heflinger <jasonheflinger@gmail.com>
2023-12-29 08:06:22 -0500

    got linux sdk set up for integration

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <Flora.h>
+#include <Flora/Core/EntryPoint.h>
+//#include "GameLayer.h"
+
+namespace Flora {
+	class Game : public Application {
+	public:
+		Game(const WindowProps& props, ApplicationCommandLineArgs args)
+			: Application(props, "", args) {
+			//PushLayer(new UCG::GameLayer());
+			//GetWindow().SetWindowIcon("assets/templogo.png");
+		}
+
+		~Game() {
+
+		}
+	};
+
+	Application* CreateApplication(ApplicationCommandLineArgs args) {
+		WindowProps props;
+		props.Title = "Untitled Card Game";
+		props.Width = 1600;
+		props.Height = 900;
+		props.Fullscreen = false;
+		return new Game(props, args);
+	}
+}
